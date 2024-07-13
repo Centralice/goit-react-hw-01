cf1cd3a26911516f265360433ebec481c243480d
Centralice <108938330+Centralice@users.noreply.github.com>
2024-07-13 17:32:21 +0200

    Structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
+import FriendList from "./FriendList/FriendList";
 import Profile from "./Profile/Profile";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
 const App = () => {
   return (
     <div>
       <Profile />
+      <FriendList />
+      <TransactionHistory />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,7 @@
+const FriendList = () => {
+  return (
+    <div>
+      <h1>Friend List</h1>
+    </div>);}
+
+    export default FriendList;
